ff6d7f23140f91b264b6172ccf1b32db42da9f75
sleon <sleon@sleon.dyndns.org>
2006-01-06 23:56:02 +0000

    added version info to viz
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/apps/freeviz@7778 67a373e5-eb02-0410-a15c-ee090a768436

--- gen.py
+++ gen.py
@@ -3,12 +3,13 @@ from sqlobject import *
 import sys,os
 import pydot
 import sys
+import re
 
 sys.path+=['/home/sleon/public_html/bla/']
 from db import *
 
 def gentopology():
-
+	regver = re.compile('.*,(\d+)')
 	#NodePair.createTable( ifNotExists=True )
 	node_pairs = list(NodePair.select())
 	nodes = list(Node.select())
@@ -16,13 +17,20 @@ def gentopology():
 
 	
 	for node in nodes:
+		matc = regver.match(node.version)
+		if matc:
+			nversion = matc.group(1)
+		else:
+			nversion = '0'
+
 		gnode = pydot.Node(node.name,  label='''\
 <<FONT POINT-SIZE="10px"><table CELLBORDER="0" border="0"
   CELLPADDING="0"
   CELLSPACING="3"
 ><tr><td align="left">%s</td>
 </tr><tr><td align="left">%s</td></tr>
-</table></FONT>>''' % (node.name,node.location[0:7]) )
+<tr><td>Ver. %s</td></tr>
+</table></FONT>>''' % (node.name,node.location[0:7],nversion ))
 		g.add_node(gnode)
 	
 	#there are no dublicate edges in the database 
